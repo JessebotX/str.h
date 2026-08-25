db7c0220ec5393937a118650ecda00818aef6824
Jesse <jessebot.git@gmail.com>
2026-08-25 00:53:48 -0700

    add cstrbuf new and delete functions

--- str.h
+++ str.h
@@ -2,23 +2,30 @@
 #define STR_H
 
 #ifndef STR_DEF
-#define STR_DEF
+   #define STR_DEF
 #endif
 
 #ifndef STR_INTERNAL
-#define STR_INTERNAL static
+   #define STR_INTERNAL static
 #endif
 
 #ifndef STR_SIZE_T
-#define STR_SIZE_T int
+   #define STR_SIZE_T int
 #endif
 
 #ifndef STR_CHAR_T
-#define STR_CHAR_T char
+   #define STR_CHAR_T char
 #endif
 
 #ifndef STR_BOOL_T
-#define STR_BOOL_T int
+   #if __STDC_VERSION__ >= 199901L
+      #if __STDC_VERSION__ < 202311L
+         #include <stdbool.h>
+      #endif
+      #define STR_BOOL_T bool
+   #else
+      #define STR_BOOL_T int
+   #endif
 #endif
 
 #if !defined(STR_MALLOC) || !defined(STR_REALLOC) || !defined(STR_FREE)
@@ -31,7 +38,7 @@
    #ifndef STR_REALLOC
       #define STR_REALLOC(ptr, n, n_old) \
          do {                            \
-            (void)n_old;                 \
+            (void)(n_old);               \
             realloc((ptr), (n));         \
          } while (0)
    #endif
@@ -39,26 +46,31 @@
    #ifndef STR_FREE
       #define STR_FREE(ptr, n) \
          do {                  \
-            (void)n;           \
+            (void)(n);         \
             free((ptr));       \
          } while (0)
    #endif
 #endif
 
 #ifndef STR_STRLEN
-#define STR_ENABLE_INTERNAL_STRLEN
-#define STR_STRLEN str__strlen
+   #define STR_ENABLE_INTERNAL_STRLEN
+   #define STR_STRLEN str__strlen
+#endif
+
+#ifndef STR_MEMCPY
+   #define STR_ENABLE_INTERNAL_MEMCPY
+   #define STR_MEMCPY str__memcpy
 #endif
 
 #ifndef STR_ASSERT
-#include <assert.h>
-#define STR_ASSERT assert
+   #include <assert.h>
+   #define STR_ASSERT assert
 #endif
 
 #define STR__EMPTY_VALUE {0}
 
 #ifndef NULL
-#define NULL ((void*)0)
+   #define NULL ((void*)0)
 #endif
 
 struct str {
@@ -81,16 +93,27 @@ struct cstrbuf {
 extern "C" {
 #endif
 
-STR_DEF struct str str_from_bytes(const CHAR_T* bytes);
-STR_DEF struct str str_from_bytes_n(const CHAR_T* bytes, STR_SIZE_T len);
+STR_DEF struct str str_from_bytes(const STR_CHAR_T* bytes);
+STR_DEF struct str str_from_bytes_n(const STR_CHAR_T* bytes, STR_SIZE_T len);
 STR_DEF struct str str_from_cstr(struct cstr s);
 
-STR_DEF struct cstr cstr_from_bytes(const CHAR_T* bytes);
+STR_DEF struct cstr cstr_from_bytes(const STR_CHAR_T* bytes);
 
-STR_INTERNAL struct cstr cstr__from_bytes_n(const CHAR_T* bytes, STR_SIZE_T len);
+STR_DEF struct cstrbuf cstrbuf_from_bytes(const STR_CHAR_T* bytes);
+STR_DEF struct cstrbuf cstrbuf_from_bytes_n(const STR_CHAR_T* bytes, STR_SIZE_T len);
+STR_DEF struct cstrbuf cstrbuf_from_bytes_reserve(const STR_CHAR_T* bytes, STR_SIZE_T len, STR_SIZE_T cap);
+STR_DEF struct cstrbuf cstrbuf_from_str(struct str s);
+STR_DEF struct cstrbuf cstrbuf_clone(struct cstrbuf s);
+STR_DEF void cstrbuf_delete(struct cstrbuf* s);
+
+STR_INTERNAL struct cstr cstr__from_bytes_n(const STR_CHAR_T* bytes, STR_SIZE_T len);
 
 #ifdef STR_ENABLE_INTERNAL_STRLEN
-STR_INTERNAL STR_SIZE_T str__strlen(const CHAR_T* bytes);
+STR_INTERNAL STR_SIZE_T str__strlen(const STR_CHAR_T* bytes);
+#endif
+
+#ifdef STR_ENABLE_INTERNAL_MEMCPY
+STR_INTERNAL void* str__memcpy(void* destination, const void* source, STR_SIZE_T n);
 #endif
 
 #ifdef __cplusplus
@@ -101,14 +124,14 @@ STR_INTERNAL STR_SIZE_T str__strlen(const CHAR_T* bytes);
 
 #ifdef STR_IMPLEMENTATION
 
-STR_DEF struct str str_from_bytes(const CHAR_T* bytes)
+STR_DEF struct str str_from_bytes(const STR_CHAR_T* bytes)
 {
    return str_from_bytes_n(bytes, STR_STRLEN(bytes));
 }
 
-STR_DEF struct str str_from_bytes_n(const CHAR_T* bytes, STR_SIZE_T len)
+STR_DEF struct str str_from_bytes_n(const STR_CHAR_T* bytes, STR_SIZE_T len)
 {
-   struct str result;
+   struct str result = STR__EMPTY_VALUE;
    STR_ASSERT(len >= 0);
 
    result.bytes = bytes;
@@ -121,9 +144,9 @@ STR_DEF struct str str_from_cstr(struct cstr s)
    return str_from_bytes_n(s.bytes, s.len);
 }
 
-STR_DEF struct cstr cstr_from_bytes(const CHAR_T* bytes)
+STR_DEF struct cstr cstr_from_bytes(const STR_CHAR_T* bytes)
 {
-   struct cstr result;
+   struct cstr result = STR__EMPTY_VALUE;
 
    result.bytes = bytes;
    result.len = STR_STRLEN(bytes);
@@ -131,7 +154,58 @@ STR_DEF struct cstr cstr_from_bytes(const CHAR_T* bytes)
    return result;
 }
 
-STR_INTERNAL struct cstr cstr__from_bytes_n(const CHAR_T* bytes, STR_SIZE_T len)
+STR_DEF struct cstrbuf cstrbuf_from_bytes(const STR_CHAR_T* bytes)
+{
+   return cstrbuf_from_bytes_n(bytes, STR_STRLEN(bytes));
+}
+
+STR_DEF struct cstrbuf cstrbuf_from_bytes_n(const STR_CHAR_T* bytes, STR_SIZE_T len)
+{
+   return cstrbuf_from_bytes_reserve(bytes, len, len + 1);
+}
+
+STR_DEF struct cstrbuf cstrbuf_from_bytes_reserve(const STR_CHAR_T* bytes, STR_SIZE_T len, STR_SIZE_T cap)
+{
+   struct cstrbuf result = STR__EMPTY_VALUE;
+   STR_ASSERT(len >= 0);
+   STR_ASSERT(cap >= 0);
+   if (len >= cap) {
+      return result;
+   }
+
+   result.bytes = (STR_CHAR_T*)STR_MALLOC(cap * sizeof(*result.bytes));
+   result.cap = cap;
+   STR_MEMCPY(result.bytes, bytes, len);
+   result.len = len;
+   result.bytes[result.len] = '\0';
+   return result;
+}
+
+STR_DEF struct cstrbuf cstrbuf_from_str(struct str s)
+{
+   return cstrbuf_from_bytes_n(s.bytes, s.len);
+}
+
+STR_DEF struct cstrbuf cstrbuf_clone(struct cstrbuf s)
+{
+   return cstrbuf_from_bytes_reserve(s.bytes, s.len, s.cap);
+}
+
+STR_DEF void cstrbuf_delete(struct cstrbuf* s)
+{
+   if (!s) {
+      return;
+   }
+
+   if (s->bytes) {
+      STR_FREE(s->bytes, s->cap * sizeof(*s->bytes));
+      s->bytes = NULL;
+   }
+   s->len = 0;
+   s->cap = 0;
+}
+
+STR_INTERNAL struct cstr cstr__from_bytes_n(const STR_CHAR_T* bytes, STR_SIZE_T len)
 {
    struct cstr result;
    STR_ASSERT(len >= 0);
@@ -143,7 +217,7 @@ STR_INTERNAL struct cstr cstr__from_bytes_n(const CHAR_T* bytes, STR_SIZE_T len)
 }
 
 #ifdef STR_ENABLE_INTERNAL_STRLEN
-STR_INTERNAL STR_SIZE_T str__strlen(const CHAR_T* bytes)
+STR_INTERNAL STR_SIZE_T str__strlen(const STR_CHAR_T* bytes)
 {
    STR_SIZE_T len = 0;
    if (bytes) {
@@ -152,6 +226,17 @@ STR_INTERNAL STR_SIZE_T str__strlen(const CHAR_T* bytes)
       }
    }
    return len;
+}
+#endif
+
+#ifdef STR_ENABLE_INTERNAL_MEMCPY
+STR_INTERNAL void* str__memcpy(void* destination, const void* source, STR_SIZE_T n)
+{
+   STR_SIZE_T i;
+   for (i = 0; i < n; i++) {
+      ((char*)destination)[i] = ((const char*)source)[i];
+   }
+   return destination;
 }
 #endif
 
